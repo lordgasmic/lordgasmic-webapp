18f692c5a825d51ffb014969d586d2d6628cb389
Nick Wellman <wellman.nicholas@gmail.com>
2020-08-06 01:40:47 -0400

    inital

---
+++ src/app/pumping/pumping.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PumpingComponent } from './pumping.component';
+
+describe('PumpingComponent', () => {
+  let component: PumpingComponent;
+  let fixture: ComponentFixture<PumpingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PumpingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PumpingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pumping/pumping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pumping',
+  templateUrl: './pumping.component.html',
+  styleUrls: ['./pumping.component.scss']
+})
+export class PumpingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
