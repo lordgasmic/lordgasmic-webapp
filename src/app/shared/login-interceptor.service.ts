38f1a1caebc9724e01cac8b1d7acb67ccea4603d
Nick Wellman <wellman.nicholas@gmail.com>
2020-11-03 16:13:40 -0500

    add login interceptor logic

--- src/app/shared/login-interceptor.service.ts
+++ src/app/shared/login-interceptor.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginInterceptorService {
+export class LoginInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  private readonly LOGIN_HEADER = "Login-Header";
+
+  constructor(private window: Window) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.headers.has(this.LOGIN_HEADER)) {
+      this.window.location.href = req.headers.get(this.LOGIN_HEADER);
+      return of(undefined);
+    }
+    return next.handle(req);
+  }
 }
