adce04f32024d163e56ee6f58a2efa16ea1ea336
Nick Wellman <wellman.nicholas@gmail.com>
2021-02-25 10:43:31 -0500

    add swgoh counters

--- src/app/approved-counters/approved-counters.component.ts
+++ src/app/approved-counters/approved-counters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Swgoh} from "../models/Swgoh";
 
 @Component({
   selector: 'app-approved-counters',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApprovedCountersComponent implements OnInit {
 
+  teams: Swgoh[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.teams.push({name:"rey", png:"rey.png", counters:[]});
+    this.teams.push({name:"cls", png:"cls.png", counters:[]});
+    this.teams.push({name:"dr", png:"dr.png", counters:[]});
+    this.teams.push({name:"gas", png:"gas.png", counters:[]});
+    this.teams.push({name:"gg", png:"gg.png", counters:[]});
+    this.teams.push({name:"geos", png:"geos.png", counters:[]});
   }
 
 }
