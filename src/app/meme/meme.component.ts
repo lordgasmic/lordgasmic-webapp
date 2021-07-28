910947443b660511984a6ba171b1a79db35bd12c
Nick Wellman <wellman.nicholas@gmail.com>
2021-07-28 17:07:01 -0400

    update router, prettier

--- src/app/meme/meme.component.ts
+++ src/app/meme/meme.component.ts
@@ -1,7 +1,7 @@
-import {ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import { LordgasmicService } from '../services/lordgasmic/lordgasmic.service';
 import { MemeResponse } from '../models/MemeResponse';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-meme',
@@ -15,16 +15,15 @@ export class MemeComponent implements OnInit {
 
   hidden: boolean;
 
-  constructor(private lordgasmicService: LordgasmicService, private route: ActivatedRoute,private router: Router, private zone: NgZone) {}
+  constructor(private lordgasmicService: LordgasmicService, private route: ActivatedRoute, private router: Router, private zone: NgZone) {}
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe(params => {
+    this.route.queryParamMap.subscribe((params) => {
       console.log(params);
       if (params.keys.length === 0) {
-        console.log("no length");
+        console.log('no length');
         this.hidden = true;
-      }
-      else {
+      } else {
         console.log(params.keys);
         const tag = params.get('tag');
         this.lordgasmicService.getMemes(tag).subscribe((value) => {
@@ -39,6 +38,6 @@ export class MemeComponent implements OnInit {
     $event.preventDefault();
 
     const tag = this.search.nativeElement.value;
-    this.zone.run(() => this.router.navigate([`/memes?tag=${tag}`]));
+    this.zone.run(() => this.router.navigate([`/memes`], { queryParams: { tag } }));
   }
 }
