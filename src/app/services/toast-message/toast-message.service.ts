e26c63af4772ffbd2c03b69f924a7d18302874f7
Nick Wellman <wellman.nicholas@gmail.com>
2021-07-30 15:57:07 -0400

    debug snackbar

--- src/app/services/toast-message/toast-message.service.ts
+++ src/app/services/toast-message/toast-message.service.ts
@@ -15,7 +15,7 @@ export class ToastMessageService {
   }
 
   showToastMessage(message: string, duration?: number): void {
-    this.matSnackBar.open(message, undefined, { duration: duration || 3000, verticalPosition: 'top', horizontalPosition: 'right' });
+    this.matSnackBar.open(message, undefined, { duration: duration || 3000 });
   }
 
   hideToastMessage(): void {
