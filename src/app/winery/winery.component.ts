fa449b67e5e6b176164e23c765fbfa9a427e3bb1
Nick Wellman <wellman.nicholas@gmail.com>
2022-01-25 22:22:26 -0500

    remove debug

--- src/app/winery/winery.component.ts
+++ src/app/winery/winery.component.ts
@@ -11,26 +11,15 @@ import { WineryResponse } from '../models/WineryResponse';
 export class WineryComponent implements OnInit {
   wineryResponse: WineryResponse;
 
-  constructor(private lordgasmicService: LordgasmicService, private route: ActivatedRoute, private router: Router, private zone: NgZone) {}
+  constructor(private lordgasmicService: LordgasmicService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      console.log(params);
-      console.log(params.id);
-      console.log(params.keys);
-      console.log(params.keys.length);
-      if (params.keys.length === 0) {
-        console.log('keys zero');
-        this.zone.run(() => this.router.navigate([`/wineTasting`], {}));
-      } else {
-        console.log('found keys');
-        const id = params.get('id');
-        console.log('id: ' + id);
-        this.lordgasmicService.getWinery(id).subscribe((value) => {
-          this.wineryResponse = value;
-          console.log(this.wineryResponse);
-        });
-      }
+      const id = params.id;
+      this.lordgasmicService.getWinery(id).subscribe((value) => {
+        this.wineryResponse = value;
+        console.log(this.wineryResponse);
+      });
     });
   }
 }
