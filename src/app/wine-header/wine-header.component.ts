5578f03856b76d80d3dfd15fb9726733e671ceb6
Nick Wellman <wellman.nicholas@gmail.com>
2022-03-15 22:54:15 -0400

    debug

--- src/app/wine-header/wine-header.component.ts
+++ src/app/wine-header/wine-header.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LordgasmicService } from '../services/lordgasmic/lordgasmic.service';
-import { resolve } from '@angular/compiler-cli/src/ngtsc/file_system';
 
 @Component({
   selector: 'app-wine-header',
