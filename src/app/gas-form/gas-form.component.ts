15e8cc998e05add14ece4bb05f0c7ea25a611dc1
Nick Wellman <wellman.nicholas@gmail.com>
2022-12-30 20:46:33 -0500

    move to gas form

--- src/app/gas-form/gas-form.component.ts
+++ src/app/gas-form/gas-form.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LordgasmicService } from '../services/lordgasmic/lordgasmic.service';
+import { DatePipe } from '@angular/common';
+import { GasRequest } from '../models/GasRequest';
 
 @Component({
   selector: 'app-gas-form',
   templateUrl: './gas-form.component.html',
   styleUrls: ['./gas-form.component.scss']
 })
-export class GasFormComponent implements OnInit {
+export class GasFormComponent implements AfterViewInit {
+  @ViewChild('date') dateRef: ElementRef;
+  @ViewChild('odd') odometerRef: ElementRef;
+  @ViewChild('vehicle') vehicleRef: ElementRef;
+  @ViewChild('gas') gasRef: ElementRef;
+  @ViewChild('cost') costRef: ElementRef;
 
-  constructor() { }
+  date: string;
 
-  ngOnInit(): void {
+  constructor(private lordgasmicService: LordgasmicService, private datePipe: DatePipe) {
+    const myDate = new Date();
+    this.date = this.datePipe.transform(myDate, 'yyyy-MM-dd');
   }
 
+  ngAfterViewInit(): void {
+    this.dateRef.nativeElement.value = this.date;
+  }
+
+  submit(): void {
+    const date = this.dateRef.nativeElement.value;
+    const odometer = this.odometerRef.nativeElement.value;
+    const vehicle = this.vehicleRef.nativeElement.value;
+    const gas = this.gasRef.nativeElement.value;
+    const cost = this.costRef.nativeElement.value;
+
+    const request: GasRequest = {
+      date,
+      odometer,
+      vehicle,
+      gas,
+      cost
+    };
+
+    this.lordgasmicService.addGas(request).subscribe((response) => {
+      window.location.reload();
+    });
+  }
 }
