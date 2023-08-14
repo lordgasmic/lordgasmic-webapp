6e9dbe51ee53af3ad5e39fdd9aeda1392db6e4c8
kerrygethere <kelly.behr87@gmail.com>
2023-08-14 15:54:41 -0400

    pretty login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,26 +2,33 @@ import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core'
 import { LoginRequest } from '../models/LoginRequest';
 import { LordgasmicService } from '../services/lordgasmic/lordgasmic.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  @ViewChild('username') username: ElementRef;
-  @ViewChild('password') password: ElementRef;
+export class LoginComponent {
 
-  constructor(private lordgasmicService: LordgasmicService, private router: Router, private zone: NgZone) {}
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
 
-  ngOnInit(): void {}
+  constructor(
+    private lordgasmicService: LordgasmicService, 
+    private router: Router, 
+    private fb: FormBuilder,
+    private zone: NgZone
+  ) {}
 
   login(): void {
-    const loginRequest = new LoginRequest();
-    loginRequest.username = this.username.nativeElement.value;
-    loginRequest.password = this.password.nativeElement.value;
-
-    this.lordgasmicService.login(loginRequest).subscribe((value) => {
+    if (!this.form.valid) {
+      return;
+    }
+    
+    this.lordgasmicService.login(this.form.value as LoginRequest).subscribe((value) => {
       this.zone.run(() => this.router.navigate(['/portal']));
     });
   }
