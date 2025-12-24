45b44d0cefe8c01419fa825e0651a0adcd90b68d
Nick Wellman <wellman.nicholas@gmail.com>
2025-12-23 22:25:31 -0500

    redo form

--- src/app/ordering/ordering.component.ts
+++ src/app/ordering/ordering.component.ts
@@ -1,11 +1,9 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { OrderingService } from '../services/ordering/ordering.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { OrderingRequest } from '@models/OrderingRequest';
 import { PrintType } from '@models/PrintType';
 import { ToastMessageService } from '../services/toast-message/toast-message.service';
 import { Router } from '@angular/router';
-import { OrderingForm } from '@models/OrderingForm';
 import { OrderingOptions } from '@models/OrderingOptions';
 
 @Component({
@@ -14,7 +12,9 @@ import { OrderingOptions } from '@models/OrderingOptions';
   styleUrls: ['./ordering.component.scss']
 })
 export class OrderingComponent implements OnInit {
-  formGroup: FormGroup<OrderingForm>;
+  @ViewChildren('menu')
+  children!: QueryList<ElementRef<HTMLInputElement>>;
+
   orderingOptions: OrderingOptions[] = [
     { name: 'Water', value: 'WATER', options: ['Ice', 'Stanley'] },
     { name: 'Salty Snacks', value: 'SALTY_SNACKS', options: ['Chips', 'Nuts'] },
@@ -27,41 +27,20 @@ export class OrderingComponent implements OnInit {
     private orderingService: OrderingService,
     private router: Router,
     private zone: NgZone,
-    private toastService: ToastMessageService,
-    private fb: FormBuilder
-  ) {
-    this.formGroup = this.fb.group({
-      rows: this.fb.array([
-        this.fb.group({
-          name: [''],
-          selectedOption: [false],
-          additionalOptions: this.fb.array<boolean>([]),
-          writeIn: ['']
-        })
-      ])
-    });
-  }
+    private toastService: ToastMessageService
+  ) {}
 
-  ngOnInit(): void {
-    // todo init form
-    for (let i = 0; i <= this.orderingOptions.length; i++) {
-      this.formGroup.controls.rows.value[i].name = this.orderingOptions[i].name;
-      this.formGroup.controls.rows.value[i].selectedOption = false;
-      // this.formGroup.controls.rows.value[i].additionalOptions = this.orderingOptions[i].name;
-      this.formGroup.controls.rows.value[i].writeIn = '';
-    }
-  }
+  ngOnInit(): void {}
 
   submit(): void {
     const properties: { [key: string]: string[] } = {};
-    console.log(this.formGroup);
-    this.formGroup.value.rows.forEach((row, i) => {
-      console.log(i);
-      console.log(row);
-      if (row.selectedOption) {
-        properties[this.orderingOptions[i].value] = [];
+
+    this.children.forEach((elem) => {
+      if (elem.nativeElement.checked) {
+        properties[elem.nativeElement.value] = [];
       }
     });
+
     const orderingRequest: OrderingRequest = { message: 'Wifey needy', type: PrintType.RECEIPT.toString(), properties };
 
     this.orderingService.placeOrder(orderingRequest).subscribe(() => {
