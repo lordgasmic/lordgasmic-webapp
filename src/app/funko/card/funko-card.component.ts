7427cea3483236284e5998820881d150337d9c36
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-27 22:47:12 -0500

    fix selector

--- src/app/funko/card/funko-card.component.ts
+++ src/app/funko/card/funko-card.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Funko } from '@models/funkos/Funko';
 
 @Component({
-  selector: 'app-card',
+  selector: 'app-funko-card',
   templateUrl: './funko-card.component.html',
   styleUrls: ['./funko-card.component.scss']
 })
