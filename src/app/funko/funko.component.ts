070e581a6b3933c3dfaaa6301c2866b1dc976750
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-27 22:38:27 -0500

    move funko logic, make root component router

--- src/app/funko/funko.component.ts
+++ src/app/funko/funko.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FunkoService } from '../services/funko/funko.service';
-import { FunkosResponse } from '@models/funkos/FunkosResponse';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-funko',
   templateUrl: './funko.component.html',
   styleUrls: ['./funko.component.scss']
 })
-export class FunkoComponent implements OnInit {
-  funkos: FunkosResponse;
-
-  constructor(private funkoService: FunkoService) {}
-
-  ngOnInit(): void {
-    this.funkoService.getFunkos().subscribe((data) => {
-      if (data) {
-        this.funkos = data;
-      }
-    });
-  }
+export class FunkoComponent {
+  constructor() {}
 }
--- src/app/funko/main/funko-main.component.ts
+++ src/app/funko/main/funko-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FunkosResponse } from '@models/funkos/FunkosResponse';
+import { FunkoService } from '../../services/funko/funko.service';
 
 @Component({
   selector: 'app-main',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funko-main.component.scss']
 })
 export class FunkoMainComponent implements OnInit {
-  constructor() {}
+  funkos: FunkosResponse;
 
-  ngOnInit(): void {}
+  constructor(private funkoService: FunkoService) {}
+
+  ngOnInit(): void {
+    this.funkoService.getFunkos().subscribe((data) => {
+      if (data) {
+        this.funkos = data;
+      }
+    });
+  }
 }
